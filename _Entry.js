e26d5a4c475fc1f845b66185382f46515f2fa876
shin039 <life.syncopation.4003@gmail.com>
2021-08-09 17:01:58 +0900

    change entry point.

---
+++ _Entry.js
@@ -0,0 +1,6 @@
+// Copy From node_modules/expo/AppEntry.js
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+import App from 'orisuke/src/main';
+
+registerRootComponent(App);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { StatusBar }                      from 'expo-status-bar';
 import React                              from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import {st_main}                          from 'orisuke/styles/main_styles';
+import {st_main}                          from 'orisuke/src/styles/main_styles';
 
 // -----------------------------------------------------------------------------
 // Const
