6da768cb525a019b0d78332364eb83fca34b431f
shin039 <life.syncopation.4003@gmail.com>
2021-08-09 16:48:56 +0900

    first commit.

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar }                      from 'expo-status-bar';
+import React                              from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {st_main}                          from 'orisuke/styles/main_styles';
 
+// -----------------------------------------------------------------------------
+// Const
+// -----------------------------------------------------------------------------
+const styles = StyleSheet.create(st_main);
+
+// -----------------------------------------------------------------------------
+// Main
+// -----------------------------------------------------------------------------
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Button color="#8e8e8e" onPress={()=>{console.log("Push!")}} title="Push!" />
+      {/* これはいるのか？ */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/main_styles.js
@@ -0,0 +1,11 @@
+// -----------------------------------------------------------------------------
+// Style Sheet Define
+// -----------------------------------------------------------------------------
+export const st_main = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}
